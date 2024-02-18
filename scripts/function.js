f48577eb6f1e31ffc557dfd46f53c9d44912ba6b
shifa <shifa.eee@gmail.com>
2024-02-19 04:20:34 +0600

    tickets updated

---
+++ scripts/function.js
@@ -0,0 +1,4 @@
+function setBackgroundColorById(elementId){
+    const element=document.getElementById(elementId) ;
+    element.classList.add('bg-lime-600');
+  }
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,51 @@
 //console.log("connected")
-
+let count=0;
+let price=0;
 const seatNo=document.querySelectorAll(".A");
 //console.log(seatNo);
 for (let index = 0; index < seatNo.length; index++) {
     const seatNumber = seatNo[index];  
     //console.log(seatNumber)
     seatNumber.addEventListener("click", function(){
-        //console.log("clicked");
         //get the text
-        const title=seatNumber.querySelector("p");
-        console.log(title.innerText);
+        const title=seatNumber.querySelector("p").innerText;
+        count++;
+        //console.log(title.innerText);
+        setBackgroundColorById(title);
+        if(count>=4){
+            alert("don't select again");
+           
+        }
+        console.log('count', count);
+      //get the price
+const price=parseFloat(document.getElementById("seatPrice").innerText.split(" ")[0]);
+// const totalPrice=price++;
+// console.log(totalPrice);
+
+
+
+//console.log(price);
+////set table data
+// const tableData=document.querySelector("tbody");
+// //console.log(tableData)
+// const data=tableData.querySelectorAll("tr");
+// //console.log(data[0]);
+// const data1=document.createElement("td");
+// //console.log(data1)
+// data[0].appendChild(data1);
+// // data[0].innerText=title;
+// // data[1].innerText="economy";
+// // data[2].innerText=price;
+// data1.innerText=title;
+// const data2=document.createElement("td");
+// data2.innerText="economy";
+// console.log(data)
+
     })
 }
+
+
+
 // function Selected(){
     
    
